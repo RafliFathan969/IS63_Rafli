66993fa68c92757348437929cb87e45abd03cb93
putrafahrezi <putrafahrezi89@gmail.com>
2025-09-26 10:56:10 +0700

    dasar php

--- index.php
+++ index.php
@@ -8,7 +8,20 @@
 </head>
 
 <body>
+    <?php
+    // echo "Hello World";
 
+    // variabel (menampung nilai)
+    $nilai1 = 70;
+    $nilai2 = 80;
+    $nama = "Rafli";
+    $kelas = "IS63";
+    $predikat = "Cumlaude";
+    $rata2 = ($nilai1 + $nilai2) / 2;
+
+    echo "Nama : <b>$nama</b> dari kelas $kelas dengan nilai rata rata nya adalah <b>$rata2</b> dengan predikat <b>$predikat</b>";
+
+    ?>
 </body>
 
 </html>
