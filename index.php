d9e0498ab332dfb0a0f3b37a0ec0b1662d08d194
RafliFathan969 <kyzenn40@gmail.com>
2025-10-10 08:57:14 +0700

    if part 1

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Web Programing</title>
+</head>
+
+<body>
+    <?php
+    // echo "Hello World";
+
+    // variabel (menampung nilai)
+    $nilai1 = 70;
+    $nilai2 = 80;
+    $nama = "Rafli";
+    $kelas = "IS63";
+    $predikat = "Cumlaude";
+    $rata2 = ($nilai1 + $nilai2) / 2;
+
+    echo "Nama : <b>$nama</b> dari kelas $kelas dengan nilai rata rata nya adalah <b>$rata2</b> dengan predikat <b>$predikat</b>";
+
+    ?>
+</body>
+
+</html>
