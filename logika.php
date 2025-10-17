efbf37599209707bd40fe94543a56d49f3b742cd
RafliFathan969 <kyzenn40@gmail.com>
2025-10-17 08:38:57 +0700

    switch

--- logika.php
+++ logika.php
@@ -51,6 +51,37 @@
     //         echo ", Harus Ikut Remedial";
     //     }
     // }
+
+    echo "<hr>";
+    ##### SWITCH #####
+    $hari = 8;
+    switch($hari){
+        case 1:
+            echo "Hari Minggu";
+            break;
+        case 2:
+            echo "Hari Senin";
+            break;
+        case 3:
+            echo "Hari Selasa";
+            break;
+        case 4:
+            echo "Hari Rabu";
+            break;
+        case 5:
+            echo "Hari kamis";
+            break;
+        case 6:
+            echo "Hari Jum'at";
+            break;
+        case 7:
+            echo "Hari Sabtu";
+            break;
+
+        default:
+            echo "Tidak ada hari";
+            break;
+    }
     ?>
 </body>
 </html>
