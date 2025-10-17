4b9aea1916db6c410aa153c7dd73dfe628bf1b11
RafliFathan969 <kyzenn40@gmail.com>
2025-10-17 09:51:22 +0700

    while & do while

--- looping.php
+++ looping.php
@@ -1,10 +1,23 @@
 <?php
 ### FOR ###
-// for($i=1;$i<=50;$i++){
-//     echo "Mahasiswa ke-$i <br>";
-// }
-
 for($i=10;$i>=1;$i--){
     echo "Manusia ke-$i <br>";
 }
+
+echo "<hr>";
+### WHILE ###
+$awal = 1;
+while($awal <= 10){
+    echo "Manusia ke-$awal<br>";
+    $awal++;
+}
+
+echo "<hr>";
+### DO WHILE ###
+$dw = 1;
+do{
+    echo "Manusia ke-$dw <br>";
+    $dw++;
+}while($dw <= 10);
+
 ?>
