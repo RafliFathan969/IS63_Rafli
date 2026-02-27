50abf862dccd4cd0b320ba706add4511d14c825e
RafliFathan969 <kyzenn40@gmail.com>
2026-02-27 10:34:40 +0700

    pembuatan login dan beranda aplikasi

---
+++ UAS/login.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+$pesan = "";
+if (isset($_POST['tombol'])) {
+    include_once('koneksi.php');
+
+    $username = $_POST['username'];
+    $pass = md5($_POST['password']);
+    $qry = "SELECT * FROM users WHERE username = '$username' AND pass = '$pass'";
+    $result = mysqli_query($koneksi, $qry);
+    $cek_login = mysqli_num_rows($result);
+
+    if ($cek_login == 0) {
+        $pesan = "Login gagal";
+    } else {
+        $pesan = "Login berhasil";
+        if(isset($_POST['cek']) == "yes"){
+            setcookie("coo_username",$username,time()+(3600*24*30), "/");
+            header("location:index.php");
+        }else{
+            $_SESSION['ses_username'] = $username;
+            header("location:index.php");
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body style="background-color:#232954">
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-8 m-auto">
+                <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
+                    <div class="card-header">
+                        <b>Halaman Login</b>
+                    </div>
+                    <div class="card-body">
+                        <div class="alert alert-success" role="alert">
+                            <?= $pesan?>
+                        </div>
+                        <form action="login.php" method="post">
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Username</label>
+                                <input type="text" name="username" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp">
+                                <div id="emailHelp" class="form-text"></div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" name="cek" value="yes" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" name="tombol" class="btn btn-primary">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- biodata/index.php
+++ biodata/index.php
@@ -3,7 +3,6 @@ include_once("ceklogin.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
